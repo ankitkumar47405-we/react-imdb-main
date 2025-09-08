729355c73a4d49ebf8f97251687a3cba1ebc6fcf
ankitkumar47405-we <ankitkumar47405@gmail.com>
2025-09-08 23:07:54 +0530

    seventh

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,8 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        manualChunks: undefined
+        manualChunks: undefined,
+        format: 'es'
       }
     }
   },
