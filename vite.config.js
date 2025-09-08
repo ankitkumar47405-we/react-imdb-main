c24ec1164a3b717be5435ed3c094e63ca6e8b601
ankitkumar47405-we <ankitkumar47405@gmail.com>
2025-09-08 22:48:53 +0530

    third

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,12 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   }
 })
